951f2fbdd70f7d18c069c68ce5b46c5f459b113c
kelseafunwi <funwikelseandohnwi@gmail.com>
2025-05-29 20:18:41 +0100

    Initial commit

---
+++ src/main/java/com/easyfarm/easyfarm/EasyfarmApplication.java
@@ -0,0 +1,13 @@
+package com.easyfarm.easyfarm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EasyfarmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EasyfarmApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/easyfarm/easyfarm/EasyfarmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.easyfarm.easyfarm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EasyfarmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
